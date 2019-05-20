2bb369b5579781dbe29f4cf9877590320eb13926
LucasTAlmeida <49998772+LucasTAlmeida@users.noreply.github.com>
2019-05-20 13:07:42 -0300

    ATIVIDADE 23/05

--- src/main.cpp
+++ src/main.cpp
@@ -1,286 +1,422 @@
 #include <aurora/Math.h>
+#include <aurora/Utility.h>
 #include <aurora/Vector.h>
 #include <aurora/Color.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <aurora/Matrix.h>
+#include <aurora/Image.h>
 
 #include <vector>
 #include <cmath>
 #include <algorithm>
+#include <cstdlib>
 #include <iostream>
 
 using namespace projeto1;
 
+float uniformRandom1D() {
+    return std::rand() / (RAND_MAX + 1.0);
+}
+Vector2 uniformRandom2D() {
+    return Vector2(uniformRandom1D(), uniformRandom1D());
+}
 
-
-
-struct Camera {
-     float fieldOfView;
-     Film film;
-     PMatrix3D worldMatrix;
+void stratifiedSample(int count, std::vector<Vector2> & samples) {
+    samples.reserve(count);
     
-     Camera() {}
-     Camera(float fieldOfView, Film film, PMatrix3D worldMatrix) {
-        this->fieldOfView = fieldOfView;
-        this->film = film;
-        this->worldMatrix = worldMatrix;
-    }
+    int size = std::sqrt(count);
+    float inverseSize = 1.0 / size;
     
-     virtual void lookAt(Vector3 position, Vector3 target, Vector3 up) {
-        Vector3 w = Vector3.sub(position, target).normalize();
-        Vector3 u = up.cross(w).normalize();
-        Vector3 v = w.cross(u);
+    for (int i = 0; i < count; i++) {
+        Vector2 offset(i / size, i % size);
+        Vector2 point = (offset + uniformRandom2D()) * inverseSize;
         
-        worldMatrix.set(
-            u.x, v.x, w.x, position.x,
-            u.y, v.y, w.y, position.y,
-            u.z, v.z, w.z, position.z,
-            0, 0, 0, 1.0);
-    }
-    virtual void transform(PMatrix3D transformation) {
-        worldMatrix.preApply(transformation);
-    }
-    
-    virtual Ray generateRay(float x, float y, Vector2 sample) {
-        float scale = tan(fieldOfView * 0.5);
-        
-        Vector2 pixel = new Vector2();
-        
-        pixel.x = (2.0 * (x + 0.5 + sample.x) / film.width - 1.0) * scale * film.aspectRatio();
-        pixel.y = (1.0 - 2.0 * (y + 0.5 + sample.y) / film.height) * scale;
-              
-        worldMatrix.mult(pixel, pixel);
-        
-        Vector2 origin = new Vector2(worldMatrix.m02, worldMatrix.m12);
-        Vector2 direction = Vector2.sub(pixel, origin).normalize();
-        
-        return new Ray(origin, direction);
+        samples.push_back(point);
     }
 }
 
-struct Film {
-     float width;
-     float height;
+float gaussian1D(float sample, float width) {
+    float radius = width * 0.5;
+    return std::fmax(0, std::exp(-sample * sample) - std::exp(-radius * radius));
+}
+float gaussian2D(const Vector2 & sample, float width) {
+    return gaussian1D(sample.x, width) * gaussian1D(sample.y, width);
+}
+
+struct Ray {
+    Vector3 origin;
+    Vector3 direction;
     
-     Film() {}
-     Film(float width, float height) {
+    Ray() {}
+    Ray(const Vector3 & origin, const Vector3 & direction) {
+        this->origin = origin;
+        this->direction = direction;
+    }
+    
+    Vector3 point(float distance) const {
+        return origin + direction * distance;
+    }
+};
+
+struct Intersection {
+    bool hit;
+    float distance;
+    int index;
+    
+    Intersection() {
+        hit = false;
+        distance = PROJETO1_INFINITY;
+        index = -1;
+    }
+    Intersection(bool hit, float distance, int index) {
+        this->hit = hit;
+        this->distance = distance;
+        this->index = index;
+    }
+};
+
+struct ShaderGlobals {
+    Vector3 point;
+    Vector3 normal;
+    Vector2 uv;
+    Vector3 tangentU;
+    Vector3 tangentV;
+    Vector3 viewDirection;
+    Vector3 lightDirection;
+    Vector3 lightPoint;
+    Vector3 lightNormal;
+    
+    ShaderGlobals() {}
+    ShaderGlobals(
+            const Vector3 & point, const Vector3 & normal, const Vector2 & uv,
+            const Vector3 & tangentU, const Vector3 & tangentV,
+            const Vector3 & viewDirection, const Vector3 & lightDirection,
+            const Vector3 & lightPoint, const Vector3 & lightNormal) {
+        this->point = point;
+        this->normal = normal;
+        this->uv = uv;
+        this->tangentU = tangentU;
+        this->tangentV = tangentV;
+        this->viewDirection = viewDirection;
+        this->lightDirection = lightDirection;
+        this->lightPoint = lightPoint;
+        this->lightNormal = lightNormal;
+    }
+};
+
+enum BSDFType {
+    Light = 0,
+    Diffuse,
+    Specular,
+    None
+};
+
+struct BSDF {
+    BSDFType type;
+    Color3 color;
+    
+    BSDF() {}
+    BSDF(BSDFType type, const Color3 & color) {
+        this->type = type;
+        this->color = color;
+    }
+};
+
+struct Shape {
+    BSDF * bsdf;
+    
+    Shape() {}
+    Shape(BSDF * bsdf) {
+        this->bsdf = bsdf;
+    }
+    
+    virtual bool intersects(const Ray & ray, Intersection & intersection) const = 0;
+    virtual void calculateShaderGlobals(
+            const Ray & ray, const Intersection & intersection,
+            ShaderGlobals & shaderGlobals) const = 0;
+    virtual float surfaceArea() const = 0;
+};
+
+struct Sphere : Shape {
+    Vector3 position;
+    float radius;
+    
+    Sphere() : Shape() {}
+    Sphere(const Vector3 & position, float radius, BSDF * bsdf) : Shape(bsdf) {
+        this->position = position;
+        this->radius = radius;
+    }
+    
+    virtual bool intersects(const Ray & ray, Intersection & intersection) const {
+        Vector3 l = position - ray.origin;
+        float t = l.dot(ray.direction);
+        
+        if (t < 0)
+            return false;
+            
+        float d2 = l.dot(l) - t * t;
+        float r2 = radius * radius;
+        
+        if (d2 > r2)
+            return false;
+        
+        float dt = std::sqrt(r2 - d2);
+        
+        float t0 = t - dt;
+        float t1 = t + dt;
+        
+        if (t0 > t1)
+            std::swap(t0, t1);
+        
+        if (t0 < 0) {
+            t0 = t1;
+            
+            if (t0 < 0)
+                return false;
+        }
+        
+        intersection.hit = true;
+        intersection.distance = t0;
+        
+        return true;
+    }
+    virtual void calculateShaderGlobals(
+            const Ray & ray, const Intersection & intersection,
+            ShaderGlobals & shaderGlobals) const {
+        shaderGlobals.point = ray.point(intersection.distance);
+        shaderGlobals.normal = (shaderGlobals.point - position).normalize();
+        
+        float theta = std::atan2(shaderGlobals.normal.x, shaderGlobals.normal.z);
+        float phi = std::acos(shaderGlobals.normal.y);
+        
+        shaderGlobals.uv.x = theta * PROJETO1_INV_PI * 0.5;
+        shaderGlobals.uv.y = phi * PROJETO1_INV_PI;
+        
+        shaderGlobals.tangentU.x = std::cos(theta);
+        shaderGlobals.tangentU.y = 0;
+        shaderGlobals.tangentU.z = -std::sin(theta);
+        
+        shaderGlobals.tangentV.x = std::sin(theta) * std::cos(phi);
+        shaderGlobals.tangentV.y = -std::sin(phi);
+        shaderGlobals.tangentV.z = std::cos(theta) * std::cos(phi);
+    }
+    virtual float surfaceArea() const {
+        return 4.0 * PROJETO1_PI * radius * radius;
+    }
+};
+
+struct Scene {
+    std::vector<Shape *> shapes;
+    
+    Scene();
+    Scene(const std::vector<Shape *> & shapes) {
+        this->shapes = shapes;
+    }
+    
+    bool intersects(const Ray & ray, Intersection & intersection) const {
+        for (int i = 0; i < shapes.size(); i++) {
+            Shape * shape = shapes[i];
+            
+            Intersection temp;
+            shape->intersects(ray, temp);
+            
+            if (temp.hit && temp.distance < intersection.distance) {
+                intersection.hit = temp.hit;
+                intersection.distance = temp.distance;
+                intersection.index = i;
+            }
+        }
+        
+        return intersection.hit;
+    }
+};
+
+struct Film {
+    float width;
+    float height;
+    
+    Film() {}
+    Film(float width, float height) {
         this->width = width;
         this->height = height;
     }
     
-     virtual float aspectRatio() {
+    float aspectRatio() const {
         return width / height;
     }
-}
-
-struct Ray {
-	Vector3 origin;
-	Vector3 direction;
-	
-	Ray() {}
-	Ray(const Vector3 & origin, const Vector3 & direction) {
-		this->origin = origin;
-		this->direction = direction;
-	}
-	
-	Vector3 point(float distance) const {
-		return origin + direction * distance;
-	}
 };
 
-struct Intersection {
-	bool hit;
-	float distance;
-	int index;
-	
-	Intersection() {
-		hit = false;
-		distance = PROJETO1_INFINITY;
-		index = -1;
-	}
-	Intersection(bool hit, float distance, int index) {
-		this->hit = hit;
-		this->distance = distance;
-		this->index = index;
-	}
+struct Camera {
+    float fieldOfView;
+    Film film;
+    Matrix4 worldMatrix;
+    
+    Camera() {}
+    Camera(float fieldOfView, const Film & film, const Matrix4 & worldMatrix) {
+        this->fieldOfView =fieldOfView;
+        this->film = film;
+        this->worldMatrix = worldMatrix;
+    }
+    
+    void lookAt(const Vector3 & position, const Vector3 & target, const Vector3 & up) {
+        Vector3 w = (position - target).normalize();
+        Vector3 u = up.cross(w).normalize();
+        Vector3 v = w.cross(u);
+        
+        worldMatrix[0][0] = u.x;
+        worldMatrix[0][1] = u.y;
+        worldMatrix[0][2] = u.z;
+        worldMatrix[0][3] = 0;
+        
+        worldMatrix[1][0] = v.x;
+        worldMatrix[1][1] = v.y;
+        worldMatrix[1][2] = v.z;
+        worldMatrix[1][3] = 0;
+        
+        worldMatrix[2][0] = w.x;
+        worldMatrix[2][1] = w.y;
+        worldMatrix[2][2] = w.z;
+        worldMatrix[2][3] = 0;
+        
+        worldMatrix[3][0] = position.x;
+        worldMatrix[3][1] = position.y;
+        worldMatrix[3][2] = position.z;
+        worldMatrix[3][3] = 1.0;
+    }
+    Ray generateRay(float x, float y, const Vector2 & sample) const {
+        float scale = std::tan(fieldOfView * 0.5);
+        
+        Vector3 pixel;
+        
+        pixel.x = (2.0 * (x + sample.x + 0.5) / film.width - 1.0) * scale * film.aspectRatio();
+        pixel.y = (1.0 - 2.0 * (y + sample.y + 0.5) / film.height) * scale;
+        pixel.z = -1.0;
+        
+        pixel *= worldMatrix;
+        
+        Vector3 position(worldMatrix[3][0], worldMatrix[3][1], worldMatrix[3][2]);
+        Vector3 direction = (pixel - position).normalize();
+        
+        return Ray(position, direction);
+    }
 };
 
-struct ShaderGlobals {
-	Vector3 point;
-	Vector3 normal;
-	Vector2 uv;
-	Vector3 tangentU;
-	Vector3 tangentV;
-	Vector3 viewDirection;
-	Vector3 lightDirection;
-	Vector3 lightPoint;
-	Vector3 lightNormal;
-	
-	ShaderGlobals() {}
-	ShaderGlobals(
-			const Vector3 & point, const Vector3 & normal, const Vector2 & uv,
-			const Vector3 & tangentU, const Vector3 & tangentV,
-			const Vector3 & viewDirection, const Vector3 & lightDirection,
-			const Vector3 & lightPoint, const Vector3 & lightNormal) {
-		this->point = point;
-		this->normal = normal;
-		this->uv = uv;
-		this->tangentU = tangentU;
-		this->tangentV = tangentV;
-		this->viewDirection = viewDirection;
-		this->lightDirection = lightDirection;
-		this->lightPoint = lightPoint;
-		this->lightNormal = lightNormal;
-	}
+struct RenderOptions {
+    int width;
+    int height;
+    int maximumDepth;
+    int cameraSamples;
+    int lightSamples;
+    int diffuseSamples;
+    float filterWidth;
+    float gamma;
+    float exposure;
+    
+    RenderOptions() {}
+    RenderOptions(int width, int height, int maximumDepth,
+            int cameraSamples, int lightSamples, int diffuseSamples,
+            float filterWidth, float gamma, float exposure) {
+        this->width = width;
+        this->height = height;
+        this->maximumDepth = maximumDepth;
+        this->cameraSamples = cameraSamples;
+        this->lightSamples = lightSamples;
+        this->diffuseSamples = diffuseSamples;
+        this->filterWidth = filterWidth;
+        this->gamma = gamma;
+        this->exposure = exposure;
+    }
 };
 
-enum BSDFType {
-	Light = 0,
-	Diffuse,
-	Specular,
-	None
-};
-
-struct BSDF {
-	BSDFType type;
-	Color3 color;
-	
-	BSDF() {}
-	BSDF(BSDFType type, const Color3 & color) {
-		this->type = type;
-		this->color = color;
-	}
-};
-
-struct Shape {
-	BSDF * bsdf;
-	
-	Shape() {}
-	Shape(BSDF * bsdf) {
-		this->bsdf = bsdf;
-	}
-	
-	virtual bool intersects(const Ray & ray, Intersection & intersection) = 0;
-	virtual void calculateShaderGlobals(
-			const Ray & ray, const Intersection & intersection,
-			ShaderGlobals & shaderGlobals) = 0;
-	virtual float surfaceArea() = 0;
-};
-
-struct Sphere : Shape {
-	Vector3 position;
-	float radius;
-	
-	Sphere() : Shape() {}
-	Sphere(const Vector3 & position, float radius, BSDF * bsdf) : Shape(bsdf) {
-		this->position = position;
-		this->radius = radius;
-	}
-	
-	virtual bool intersects(const Ray & ray, Intersection & intersection) {
-		Vector3 l = position - ray.origin;
-		float t = l.dot(ray.direction);
-		
-		if (t < 0)
-			return false;
-			
-		float d2 = l.dot(l) - t * t;
-		float r2 = radius * radius;
-		
-		if (d2 > r2)
-			return false;
-		
-		float dt = std::sqrt(r2 - d2);
-		
-		float t0 = t - dt;
-		float t1 = t + dt;
-		
-		if (t0 > t1)
-			std::swap(t0, t1);
-		
-		if (t0 < 0) {
-			t0 = t1;
-			
-			if (t0 < 0)
-				return false;
-		}
-		
-		intersection.hit = true;
-		intersection.distance = t0;
-		
-		return true;
-	}
-	virtual void calculateShaderGlobals(
-			const Ray & ray, const Intersection & intersection,
-			ShaderGlobals & shaderGlobals) {
-		shaderGlobals.point = ray.point(intersection.distance);
-		shaderGlobals.normal = (shaderGlobals.point - position).normalize();
-		
-		float theta = std::atan2(shaderGlobals.normal.x, shaderGlobals.normal.z);
-		float phi = std::acos(shaderGlobals.normal.y);
-		
-		shaderGlobals.uv.x = theta * PROJETO1_INV_PI * 0.5;
-		shaderGlobals.uv.y = phi * PROJETO1_INV_PI;
-		
-		shaderGlobals.tangentU.x = std::cos(theta);
-		shaderGlobals.tangentU.y = 0;
-		shaderGlobals.tangentU.z = -std::sin(theta);
-		
-		shaderGlobals.tangentV.x = std::sin(theta) * std::cos(phi);
-		shaderGlobals.tangentV.y = -std::sin(phi);
-		shaderGlobals.tangentV.z = std::cos(theta) * std::cos(phi);
-	}
-	virtual float surfaceArea() {
-		return 4.0 * PROJETO1_PI * radius * radius;
-	}
-};
-
-struct Scene {
-	std::vector<Shape *> shapes;
-	
-	Scene();
-	Scene(const std::vector<Shape *> & shapes) {
-		this->shapes = shapes;
-	}
-	
-	bool intersects(const Ray & ray, Intersection & intersection) {
-		for (int i = 0; i < shapes.size(); i++) {
-			Shape * shape = shapes[i];
-			
-			Intersection temp;
-			shape->intersects(ray, temp);
-			
-			if (temp.hit && temp.distance < intersection.distance) {
-				intersection.hit = temp.hit;
-				intersection.distance = temp.distance;
-				intersection.index = i;
-			}
-		}
-		
-		return intersection.hit;
-	}
+struct Renderer {
+    RenderOptions * options;
+    Camera * camera;
+    Scene * scene;
+    
+    Renderer() {}
+    Renderer(RenderOptions * options, Camera * camera, Scene * scene) {
+        this->options = options;
+        this->camera = camera;
+        this->scene = scene;
+    }
+    
+    Color3 computeDirectIllumination(
+            const BSDF & bsdf, ShaderGlobals & shaderGlobals) const {
+        return Color3();
+    }
+    Color3 computeIndirectIllumination(
+            const BSDF & bsdf, ShaderGlobals & shaderGlobals) const {
+        return Color3();
+    }
+    
+    Color3 trace(const Ray & ray, int depth) const {
+        Intersection intersection;
+        
+        if (scene->intersects(ray, intersection))
+            return Color3(1.0, 1.0, 1.0);
+        
+        return Color3();
+    }
+    Color3 render(Image3 * image) const {
+        const Vector2 half(0.5, 0.5);
+        
+        for (int i = 0; i < options->width; i++) {
+            for (int j = 0; j < options->height; j++) {
+                std::vector<Vector2> samples;
+                stratifiedSample(options->cameraSamples, samples);
+                
+                Color3 color;
+                float weight = 0;
+                
+                for (int k = 0; k < options->cameraSamples; k++) {
+                    Vector2 sample = (samples[k] - half) * options->filterWidth;
+                    Ray ray = camera->generateRay(i, j, sample);
+                    
+                    float w = gaussian2D(sample, options->filterWidth);
+                    
+                    color += trace(ray, 0) * w;
+                    weight += w;
+                }
+                
+                color /= weight;
+                
+                color.applyExposure(options->exposure);
+                color.applyGamma(options->gamma);
+                
+                image->setPixel(i, j, color);
+            }
+        }
+    }
 };
 
 int main(int argc, char ** argv) {
-	BSDF * bsdf = new BSDF(BSDFType::Diffuse, Color3(1.0, 1.0, 1.0));
-	Shape * shape = new Sphere(Vector3(0, 0, 0), 1.0, bsdf);
-	
-	std::vector<Shape *> shapes;
-	shapes.push_back(shape);
-	
-	Scene scene(shapes);
-	
-	Ray ray(Vector3(0, 0, 10.0), Vector3(0, 0, -1.0));
-	Intersection intersection;
-	
-	scene.intersects(ray, intersection);
-	
-	std::cout << "Hit: " << intersection.hit << std::endl;
-	std::cout << "Distance: " << intersection.distance << std::endl;
-	std::cout << "Index: " << intersection.index << std::endl;
-	
-	delete bsdf;
-	delete shape;
-	
-	return 0;
+    RenderOptions options(500, 250, 1, 4, 1, 1, 2.0, 2.2, 0);
+    
+    Film film(options.width, options.height);
+    
+    Camera camera(radians(45.0), film, Matrix4());
+    camera.lookAt(Vector3(0, 0, 5.0), Vector3(0, 0, 0), Vector3(0, 1.0, 0));
+    
+    BSDF * bsdf = new BSDF(BSDFType::Diffuse, Color3(1.0, 1.0, 1.0));
+    Shape * shape = new Sphere(Vector3(0, 0, 0), 1.0, bsdf);
+    
+    std::vector<Shape *> shapes;
+    shapes.push_back(shape);
+    
+    Scene scene(shapes);
+    
+    Renderer renderer(&options, &camera, &scene);
+    
+    Image3 * image = new Image3(options.width, options.height);
+    renderer.render(image);
+    
+    if (writeImage("output.ppm", image))
+        std::cout << "Success." << std::endl;
+    else
+        std::cout << "Failure." << std::endl;
+    
+    delete bsdf;
+    delete shape;
+    delete image;
+    
+    return 0;
 }
